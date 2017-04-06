8ccab116cecc824930436060c8f6534e55b64d96
aleksatr <aleksatr@gmail.com>
2017-04-06 23:31:23 +0200

    test

--- Program.cs
+++ Program.cs
@@ -9,16 +9,35 @@ namespace Blackjack
 {
     public class Program
     {
+       public static double[] q = new double[200];
+
         public static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
             IDeck deck = new InfiniteDeck(); 
+            
+            Card dealerCard = deck.GetNextCard();
+            int dealerSum = dealerCard.BlackjackValue;
+            int playerSum = 0;
+            List<Card> playerCards = new List<Card>();
+            int usableAce = 0;
 
-            for (int i = 0; i < 52; i++)
+            while(playerSum < 12)
             {
-                Console.WriteLine(deck.GetNextCard());
+                var newCard = deck.GetNextCard();
+                playerCards.Add(newCard);
+                playerSum = playerCards.Sum(c => c.BlackjackValue);
+                int numOfAces = playerCards.Where(c => c.BlackjackValue == 1).Count();
+
+                if(playerSum <= 11 && numOfAces > 0)
+                {
+                    usableAce = 1;
+                    playerSum += 10;
+                } 
             }
+
+            
         }
     }
 }
